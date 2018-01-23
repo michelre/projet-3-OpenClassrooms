9f13bb512823e5c86ceb686aabccd59c4a8452dc
remimichel <remi.michel38@gmail.com>
2018-01-23 18:59:50 +0100

    session 2 - ajax + gmap

--- js/map.js
+++ js/map.js
@@ -1,11 +1,29 @@
-var map;
+
+
+$.getJSON('https://api.jcdecaux.com/vls/v1/stations?contract=Lyon&apiKey=00725f1585ae004d2043b59894843d43b6650b8e')
+  .then((data) => {
+    createMarkers(initMap(), data)
+  });
+
+function createMarkers(map, dataAPI){
+  // TODO
+  const markers = dataAPI.map(({ position, name }) => {
+    return new google.maps.Marker({
+      position,
+      map: map,
+      title: name,
+    })
+  });
+  new MarkerClusterer(map, markers,
+    {imagePath: 'https://developers.google.com/maps/documentation/javascript/examples/markerclusterer/m'});
+}
 
 function initMap() {
-    map = new google.maps.Map(document.querySelector("#map"), {
-        center: {
-            lat: 45.7616,
-            lng: 4.8559
-        },
-        zoom: 13
-    });
+  return new google.maps.Map(document.querySelector("#map"), {
+    center: {
+      lat: 45.7616,
+      lng: 4.8559
+    },
+    zoom: 12
+  });
 }
