07db01abeaf2a6a00d709a2b3aec6ab33a195b41
dubost-clement <braunschweig-dubost@outlook.fr>
2018-01-23 17:48:00 +0100

    ajout block reservation

--- js/map.js
+++ js/map.js
@@ -9,8 +9,3 @@ function initMap() {
         zoom: 13
     });
 }
-
-var req = new XMLHttpRequest();
-req.open("GET", "https://api.jcdecaux.com/vls/v1/stations?contract=Lyon&apiKey=00725f1585ae004d2043b59894843d43b6650b8e");
-req.send(null);
-console.log(req.responseText);
